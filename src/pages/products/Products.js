9971584497cc197bef797c25a561c32912061275
pawelkom88e <pawel.komorkiewicz@yahoo.co.uk>
2022-06-20 14:29:25 +0100

    fix: Tweaked code in other components in order to make useFetch work

--- src/pages/products/Products.js
+++ src/pages/products/Products.js
@@ -6,33 +6,42 @@ import "./Products.scss";
 
 export default function Products({data}) {
   const {category} = useParams();
-
   let filteredProducts;
 
-  if (category) {
-    filteredProducts = data.filter(productList => productList.path === `/${category}`);
+  if (data && category) {
+    filteredProducts = data.filter(productList => productList.category === `${category}`);
   } else {
     filteredProducts = data;
   }
 
   return (
     <section>
-      <h2 className="heading">Category Name</h2>
-      <div className="category-sortable-panel">
-        <div className="panel-options">
-          <span>Sort by</span>
-          <img src={sort} alt="Sort icon" />
-          <Button content="title" className="background" />
-          <Button content="price" className="background" />
-          <Button content="discount" className="background" />
-        </div>
-        <span className="product-quantity">99 items available</span>
-      </div>
-      <div className="category-products">
-        {filteredProducts.map(product => {
-          return <ProductCard key={product.id} product={product} />;
-        })}
-      </div>
+      {filteredProducts ? (
+        <>
+          <h2 className="heading">
+            {filteredProducts === data ? "All products" : filteredProducts.category}
+          </h2>
+          <div className="category-sortable-panel">
+            <div className="panel-options">
+              <span>Sort by</span>
+              <img src={sort} alt="Sort icon" />
+              <Button content="title" className="background" />
+              <Button content="price" className="background" />
+              <Button content="discount" className="background" />
+            </div>
+            <span className="product-quantity">
+              {filteredProducts.length} {filteredProducts.length > 1 ? "products" : "product"}
+            </span>
+          </div>
+          <div className="category-products">
+            {filteredProducts.map(product => {
+              return <ProductCard key={product.id} product={product} />;
+            })}
+          </div>
+        </>
+      ) : (
+        "loading"
+      )}
     </section>
   );
 }
