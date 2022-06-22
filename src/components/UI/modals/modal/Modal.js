b8b36e862a4094e53e44d95b1952d9369e5b5636
pawelkom88e <pawel.komorkiewicz@yahoo.co.uk>
2022-06-22 18:27:28 +0100

    refactor: Improve code

--- src/components/UI/modals/modal/Modal.js
+++ src/components/UI/modals/modal/Modal.js
@@ -1,22 +1,28 @@
+import useKeyPress from "hooks/useKeyPress";
 import {createPortal} from "react-dom";
 import Backdrop from "../backdrop/Backdrop";
 import React from "react";
-import "./Modal.scss";
 import CloseBtn from "components/UI/close-button/CloseBtn";
+import FocusLock from "react-focus-lock";
+import "./Modal.scss";
 
 export default function Modal({heading, children, toggle}) {
+  useKeyPress("Escape", toggle);
+
   return createPortal(
     <Backdrop toggle={toggle}>
-      <div
-        className="modal-container"
-        onClick={e => {
-          // do not close modal if anything inside modal content is clicked
-          e.stopPropagation();
-        }}>
-        <CloseBtn size={20} toggle={toggle} />
-        <h2 className="modal-heading">{heading}</h2>
-        {children}
-      </div>
+      <FocusLock>
+        <div
+          className="modal-container"
+          onClick={e => {
+            // do not close modal if anything inside modal content is clicked
+            e.stopPropagation();
+          }}>
+          <CloseBtn toggle={toggle} />
+          <h2 className="modal-heading">{heading}</h2>
+          {children}
+        </div>
+      </FocusLock>
     </Backdrop>,
     document.getElementById("modal")
   );
--- src/components/UI/modals/voucher/VoucherModal.js
+++ src/components/UI/modals/voucher/VoucherModal.js
@@ -5,18 +5,19 @@ import {CopyToClipboard} from "react-copy-to-clipboard";
 import toast, {Toaster} from "react-hot-toast";
 
 export default function VoucherModal({toggle}) {
+  const voucherCode = "HX10TZ6";
 
   function notifyUser() {
-    toast.success("Code has been copied");
+    toast.success("Code has been copied to cliboard");
   }
-  
+
   return (
     <>
       <Toaster position="top-center" />
       <Modal heading={"Your voucher code:"} toggle={toggle}>
-        <span className="voucher-code">HX10TZ6</span>
-        <CopyToClipboard text="HX10TZ6" onCopy={notifyUser}>
-          <Button className="background" content="Copy" />
+        <span className="voucher-code">{voucherCode}</span>
+        <CopyToClipboard text={voucherCode}>
+          <Button onClick={notifyUser} className="background" content="Copy code" />
         </CopyToClipboard>
       </Modal>
     </>
