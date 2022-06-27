38d161d7a6fd4e771648c0189297d62eec394036
pawelkom88e <pawel.komorkiewicz@yahoo.co.uk>
2022-06-27 14:05:35 +0100

    refactor: Add shopping cart item

--- src/pages/basket-summary/BasketSummary.js
+++ src/pages/basket-summary/BasketSummary.js
@@ -1,12 +1,21 @@
+import ShoppingCartItem from "components/UI/shopping-cart-item/ShoppingCartItem";
+import "./BasketSummary.scss";
+
 export default function BasketSummary() {
   return (
-    <section>
-      <div className="shopping-cart-item-COMPONENT">create component</div>
-      <div className="shopping-cart-item-COMPONENT">create component</div>
-      <div className="shopping-cart-item-COMPONENT">create component</div>
-      <div className="shopping-cart-item-COMPONENT">create component</div>
-      <div className="shopping-cart-item-COMPONENT">create component</div>
-      <div className="shopping-cart-item-COMPONENT">create component</div>
+    <section className="basket-summary-container">
+      <div className="basket-products">
+        <h2>Basket</h2>
+        <ShoppingCartItem />
+        <ShoppingCartItem />
+        <ShoppingCartItem />
+        <ShoppingCartItem />
+        <ShoppingCartItem />
+      </div>
+      <div className="discount-container">
+        <h2>Apply discount</h2>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi, pariatur.
+      </div>
     </section>
   );
 }
