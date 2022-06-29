cf96fb740f3334c03de8b13adc7927d2ca9e0162
pawelkom88e <pawel.komorkiewicz@yahoo.co.uk>
2022-06-29 10:58:42 +0100

    feat: Add sorting functions

--- src/pages/products/Products.js
+++ src/pages/products/Products.js
@@ -1,3 +1,4 @@
+import {useState, useEffect} from "react";
 import {useParams} from "react-router-dom";
 import ProductCard from "components/UI/product-card/ProductCard";
 import Button from "components/UI/button/Button";
@@ -7,15 +8,30 @@ import ErrorModal from "components/UI/modals/error-modal/ErrorModal";
 import "./Products.scss";
 
 export default function Products({data, error, loading}) {
+  const [filteredProducts, setFilteredProducts] = useState(null);
   const {category} = useParams();
-  let filteredProducts;
-  let categoryName;
 
-  if (data && category) {
-    filteredProducts = data.filter(productList => productList.category === category);
-    categoryName = filteredProducts[0].category.toUpperCase();
-  } else {
-    filteredProducts = data;
+  useEffect(() => {
+    function sortById(products) {
+      return products.filter(productList => productList.category === category);
+    }
+
+    if (data && category) {
+      setFilteredProducts(sortById(data));
+    } else {
+      setFilteredProducts(data);
+    }
+  }, [data, category]);
+
+  function sortByTitle(products) {
+    return products.sort((productA, productB) => productA.title.localeCompare(productB.title));
+  }
+
+  function sortByPrice(products, order) {
+    if (order === "desc") {
+      return products.sort((productA, productB) => productB.price - productA.price);
+    }
+    return products.sort((productA, productB) => productB.price + productA.price);
   }
 
   return (
@@ -24,13 +40,28 @@ export default function Products({data, error, loading}) {
       {loading && <Loader />}
       {filteredProducts && (
         <>
-          <h2 className="heading">{filteredProducts === data ? "All products" : categoryName}</h2>
+          <h2 className="heading">
+            {/* {category ? filteredProducts[0].category.toUpperCase() : "ALL PRODUCTS"} */}
+          </h2>
           <div className="category-sortable-panel">
             <div className="panel-options">
               <span>Sort by</span>
               <img src={sort} alt="Sort icon" />
-              <Button content="title" id="dark-background" />
-              <Button content="price" id="dark-background" />
+              <Button
+                content="title"
+                id="dark-background"
+                onClick={() => setFilteredProducts(sortByTitle(filteredProducts))}
+              />
+              <Button
+                content="price (cheapest)"
+                id="dark-background"
+                onClick={() => setFilteredProducts(sortByPrice(filteredProducts, "asc"))}
+              />
+              <Button
+                content="price (most expensive)"
+                id="dark-background"
+                onClick={() => setFilteredProducts(sortByPrice(filteredProducts, "desc"))}
+              />
               <Button content="discount" id="dark-background" />
             </div>
             <span className="product-quantity">
