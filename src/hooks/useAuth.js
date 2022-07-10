300254c1975023dfc7eb724f55e3ed416632e301
pawelkom88e <pawel.komorkiewicz@yahoo.co.uk>
2022-07-10 16:21:52 +0100

    feat: create signIn/logIn hook

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,20 +1,30 @@
 import {useState} from "react";
+import useAuthContext from "hooks/useAuthContext";
 import {auth} from "../firebase/config";
 import toast from "react-hot-toast";
 import {notifyUser} from "utilities/helpers";
+import {updateProfile} from "firebase/auth";
 
-export default function useAuth(method) {
+export default function useAuth(method, action) {
+  const {dispatch} = useAuthContext();
+
   const [userData, setUserData] = useState(null);
   const [loading, isLoading] = useState(false);
   const [error, setError] = useState(null);
 
-  async function handleUser(email, password, message) {
+  async function handleUser(email, password, userName = "Guest", message) {
     isLoading(true);
     try {
       const userCredential = await method(auth, email, password);
       setUserData(userCredential.user);
+      userName &&
+        updateProfile(auth.currentUser, {
+          displayName: userName,
+        });
+
       isLoading(false);
       setError(null);
+      dispatch({type: action, payload: userCredential.user});
       notifyUser(toast.success, message);
       // navigate somewhere
     } catch (error) {
