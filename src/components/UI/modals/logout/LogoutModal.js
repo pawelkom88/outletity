a2f595521d94095a16604680235d5075eb0cae4c
pawelkom88e <pawel.komorkiewicz@yahoo.co.uk>
2022-07-10 16:22:41 +0100

    feta: implement logout functionality

--- src/components/UI/modals/logout/LogoutModal.js
+++ src/components/UI/modals/logout/LogoutModal.js
@@ -1,13 +1,16 @@
+import useLogout from "hooks/useLogout";
+
 import Modal from "../modal/Modal";
 
 export default function LogoutModal({isShown, toggle}) {
+  const {logUserOut, error} = useLogout();
 
   return (
     <>
       {/* {!isLoggedOut && ( */}
-        <Modal isShown={isShown} toggle={toggle} heading={"Click to log out"}>
-          {/* <button onClick={logUserOut}>Log out</button> */}
-        </Modal>
+      <Modal isShown={isShown} toggle={toggle} heading={"Click to log out"}>
+        <button onClick={logUserOut}>Log out</button>
+      </Modal>
       {/* )} */}
     </>
   );
