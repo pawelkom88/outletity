daeb708528ca9651a0b1c22d0109cae9ce563064
pawelkom88e <pawel.komorkiewicz@yahoo.co.uk>
2022-07-11 21:22:01 +0100

    fix: remove user display name from function

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -3,25 +3,18 @@ import useAuthContext from "hooks/useAuthContext";
 import {auth} from "../firebase/config";
 import toast from "react-hot-toast";
 import {notifyUser} from "utilities/helpers";
-import {updateProfile} from "firebase/auth";
 
 export default function useAuth(method, action) {
   const {dispatch} = useAuthContext();
-
   const [userData, setUserData] = useState(null);
   const [loading, isLoading] = useState(false);
   const [error, setError] = useState(null);
-
-  async function handleUser(email, password, userName = "Guest", message) {
+  
+  async function handleUser(email, password, message) {
     isLoading(true);
     try {
       const userCredential = await method(auth, email, password);
       setUserData(userCredential.user);
-      userName &&
-        updateProfile(auth.currentUser, {
-          displayName: userName,
-        });
-
       isLoading(false);
       setError(null);
       dispatch({type: action, payload: userCredential.user});
@@ -32,5 +25,6 @@ export default function useAuth(method, action) {
       isLoading(false);
     }
   }
+
   return {handleUser, userData, loading, error};
 }
