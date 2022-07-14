a323755dffa6dbb64bf99c7c85093f1daf75e490
pawelkom88e <pawel.komorkiewicz@yahoo.co.uk>
2022-07-14 22:49:16 +0100

    feat: Add newsletter and its validation

--- src/components/UI/modals/newsletter-modal/NewsletterModal.js
+++ src/components/UI/modals/newsletter-modal/NewsletterModal.js
@@ -1,26 +1,9 @@
 import Modal from "../modal/Modal";
-import toast, {Toaster} from "react-hot-toast";
 import MailchimpSubscribe from "react-mailchimp-subscribe";
+import SubscribeForm from "components/UI/subscribe-form/SubscribeForm";
 import "./NewsletterModal.scss";
 
-const url = process.env.MAILCHIMP_URL;
-
-function Form() {
-  <MailchimpSubscribe url={url} />;
-}
-
 export default function NewsletterModal({toggle}) {
-  // // future use
-  // toast.promise(
-  //   Promise,
-  //    {
-  //      loading: 'Saving...',
-  //      success: <b>Thank you for subscribing !</b>,
-  //      error: <b>Something went wrong...Try again</b>,
-  //    }
-  //  );
-  // setTimeout(toggle, 3000);
-
   return (
     <>
       <Modal heading={"Don’t miss out !"} toggle={toggle}>
@@ -28,20 +11,17 @@ export default function NewsletterModal({toggle}) {
           Sign up for our newsletter to stay in the loop.
         </p>
         <MailchimpSubscribe
-          url={url}
+          url={process.env.REACT_APP_MAILCHIMP_URL}
+          // add reCaptcha
           render={({subscribe, status, message}) => (
-            <div>
-              <Form onSubmitted={formData => subscribe(formData)} />
-              {status === "sending" && <div style={{color: "blue"}}>sending...</div>}
-              {status === "error" && (
-                <div style={{color: "red"}} dangerouslySetInnerHTML={{__html: message}} />
-              )}
-              {status === "success" && <div style={{color: "green"}}>Subscribed !</div>}
-            </div>
+            <SubscribeForm
+              status={status}
+              message={message}
+              onValidated={formData => subscribe(formData)}
+            />
           )}
         />
       </Modal>
-      <Toaster position="top-center" />
     </>
   );
 }
