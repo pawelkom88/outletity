c9c5fbe897f5b945b4bf698c1ab604cc63d50406
pawelkom88e <pawel.komorkiewicz@yahoo.co.uk>
2022-07-16 16:34:18 +0100

    feat: Display user comments + styles

--- src/components/UI/comments/Comments.js
+++ src/components/UI/comments/Comments.js
@@ -1,50 +1,39 @@
 import {useState} from "react";
+import useCollection from "hooks/useCollection";
 import useAuthContext from "hooks/useAuthContext";
 import useModal from "hooks/useModal";
 import Button from "../button/Button";
+import UserComments from "../user-comments/userComments";
 import CommentModal from "../modals/comment-modal/CommentModal";
-import {user as x, star} from "utilities/images";
 import "./Comments.scss";
 
-export default function Comments() {
+export default function Comments({title}) {
   const {user} = useAuthContext();
   const {isShown, toggle} = useModal();
-  const [numberOfStars, setNumberOfStars] = useState(0);
   const [review, setReview] = useState("");
-
-  // 1. If user is logged in he/she can add comments
-
-  //user photo ? email
-
-  //   random color border firebase timestamp , new collections
-  // if user logged on can add comments - certain product certain ID !!!
-
-  // all users see comments
+  const {data: comments} = useCollection("COMMENTS");
 
   return (
     <div className="comments-container">
-      <h2 className="comment-heading">Customers comments</h2>
-      <div className="comments-bubble">
-        <div className="user-details">
-          <img className="user" src={x} alt="" />
-          <p>User email</p>
-          <p>Added : 2022</p>
-          <img src={star} alt="star" />
-        </div>
-        <p className="comment-content">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minus maiores, distinctio at
-          exercitationem veniam cupiditate ab natus similique hic vero tempora autem corporis
-          officiis iure architecto, recusandae quo magni excepturi.
-        </p>
-      </div>
+      <h2 className="comment-heading">Customers comments: {comments && comments.length}</h2>
+      {comments &&
+        comments.map(comment => {
+          if (comment.id.includes(title.substring(0, 30))) {
+            return (
+              <UserComments
+                key={comment.id}
+                timeStamp={comment.timestamp}
+                content={comment.content}
+                rating={comment.rating}
+                email={user?.email}
+              />
+            );
+          }
+          return null;
+        })}
       {user && <Button onClick={toggle} content="Add comment" id="dark-background" />}
       {isShown && (
-        <CommentModal
-          setReview={setReview}
-          numberOfStars={numberOfStars}
-          setNumberOfStars={setNumberOfStars}
-          toggle={toggle}
-        />
+        <CommentModal title={title} review={review} setReview={setReview} toggle={toggle} />
       )}
     </div>
   );
