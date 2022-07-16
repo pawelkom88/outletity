ede7d7ef697386c6383913320de74211e0fa4160
pawelkom88e <pawel.komorkiewicz@yahoo.co.uk>
2022-07-16 16:37:33 +0100

    refactor: remove user avatar download function and move it to custom hook

--- src/components/UI/user-panel/UserPanel.js
+++ src/components/UI/user-panel/UserPanel.js
@@ -1,42 +1,20 @@
 import {useState, useEffect} from "react";
+import UseUserAvatar from "hooks/useUserAvatar.js";
 import {user as userIcon} from "utilities/images";
 import LoginModal from "../modals/login/LoginModal";
 import SignupModal from "../modals/signup/SignupModal";
-import useAuthContext from "hooks/useAuthContext";
-import {getDownloadURL, ref} from "firebase/storage";
-import {storage} from "../../../firebase/config";
-
 import "./UserPanel.scss";
 
 export default function UserPanel({isShown, toggle}) {
-  const {user} = useAuthContext();
-  const [avatar, setAvatar] = useState(null);
   const [isUploaded, setIsUploaded] = useState(false);
   const [isSignedIn, setIsSignedIn] = useState(false);
   const [isLoggedIn, setIsLoggedIn] = useState(true);
+  const {user, avatar, setAvatar} = UseUserAvatar(isUploaded);
 
   function handleTransitionBetweenModals() {
     setIsSignedIn(prevState => !prevState);
     setIsLoggedIn(prevState => !prevState);
   }
-
-  // if user is logged in, download his avatar from firebase storage
-  useEffect(() => {
-    async function getUserAvatar(id) {
-      const userAvatar = await getDownloadURL(ref(storage, id));
-
-      try {
-        setAvatar(userAvatar);
-      } catch (error) {
-        if (error.name === "storage/object-not-found") {
-          return;
-        }
-      }
-    }
-    if (user) {
-      getUserAvatar(user.uid);
-    }
-  }, [isUploaded, user]);
 
   // back to default value after every modal close
   useEffect(() => {
@@ -64,10 +42,7 @@ export default function UserPanel({isShown, toggle}) {
       </button>
 
       {isShown && isSignedIn && (
-        <SignupModal
-          toggle={toggle}
-          handleTransition={handleTransitionBetweenModals}
-        />
+        <SignupModal toggle={toggle} handleTransition={handleTransitionBetweenModals} />
       )}
     </>
   );
