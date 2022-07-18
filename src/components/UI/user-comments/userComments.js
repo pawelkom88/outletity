b191768c56ecde6aa84ece7b29736610abfb807a
pawelkom88e <pawel.komorkiewicz@yahoo.co.uk>
2022-07-18 05:51:28 +0100

    fix: Fix the way user name is displayed in comment section

--- src/components/UI/user-comments/userComments.js
+++ src/components/UI/user-comments/userComments.js
@@ -1,15 +1,13 @@
-import UseUserAvatar from "hooks/useUserAvatar";
 import {star} from "utilities/images";
 import "./userComments.scss";
 
-export default function userComments({email, timeStamp, rating, content}) {
-  const {avatar} = UseUserAvatar(true);
-
+export default function userComments({email, avatar, timeStamp, rating, content}) {
+  const userName = email.split("@");
   return (
     <div className="comments-bubble">
       <div className="user-details">
-        <img className="comment-avatar" src={avatar} alt="" />
-        <p className="user-email">{email}</p>
+        <img className="comment-avatar" src={avatar} alt="user avatar" />
+        <p className="user-email">{userName[0]}</p>
         <p className="timestamp">Added : {timeStamp ? timeStamp.toDate().toLocaleString() : ""}</p>
         <div className="rating">
           {[...new Array(rating)].map((_, i) => (
